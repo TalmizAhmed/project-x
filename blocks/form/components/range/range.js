e971d240a9adeadcc52d9e50a0f8d9bc21a6f91f
Talmiz Ahmed <talmiz.tech@gmail.com>
2024-11-18 16:09:15 +0530

    linting issues

--- blocks/form/components/range/range.js
+++ blocks/form/components/range/range.js
@@ -1,4 +1,4 @@
 export default async function decorate(rangeDiv, fieldJson) {
-    console.log("RANGE DIV: ", rangeDiv)
-    console.log("RANGE JSON: fieldJson", fieldJson)
+  console.log('RANGE DIV: ', rangeDiv);
+  console.log('RANGE JSON: fieldJson', fieldJson);
 }
